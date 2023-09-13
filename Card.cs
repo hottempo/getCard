cf7439aa64127ddba850df84b2ca07b58536f3cf
Thomas Dall Pedersen <tempo@mail.dk>
2023-09-13 16:29:25 -0200

    done

---
+++ Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getCard
+{
+    public class Card
+    {
+        public string CardFace { get; set; }
+        public string CardValue { get; set; }
+
+        private static string[] CardFaces = { "Spar", "Hjerter", "Klør", "Ruder" };
+
+        private static IDictionary<int, string> CardValues = new Dictionary<int, string>()
+        {
+            {0, "Es" },
+            {10, "Knægt" },
+            {11, "Dame" },
+            {12, "Konge" }
+        };
+
+        public static Card FetchCard(int cardFace, int cardValue)
+        {
+            Card card = new Card();
+
+            card.CardFace = CardFaces[cardFace];
+            card.CardValue = (cardValue == 0 || cardValue > 9) ? CardValues[cardValue] : (cardValue + 1).ToString();
+
+            return card;
+        }
+    }
+}
---
+++ GetCardRandom.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getCard
+{
+    public static class GetCardRandom
+    {
+        [FunctionName("GetCardRandom")]
+        public static Card Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="GetCard/Random")] HttpRequest req)
+        {
+            Random rnd = new Random();
+            int cardFace = rnd.Next(3);
+            int cardValue = rnd.Next(12);
+
+            Card card = Card.FetchCard(cardFace, cardValue);
+
+            return card;
+
+        }
+    }
+}
